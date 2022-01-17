32a86dd138c4c999c6e0d4da4c4e78ac43238121
Wannes Dieltiens <wannesdieltiens@gmail.com>
2022-01-17 17:21:03 +0100

    updated

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 
 contract Lottery {
     address public manager;
-    address [] public players;
+    address payable[] public players;
     
     constructor() {
         manager = msg.sender;
@@ -13,19 +13,17 @@ contract Lottery {
     function enter() public payable {
         require(msg.value > .01 ether);
         
-        players.push(msg.sender);
+        players.push(payable(msg.sender));
     }
     
     function random() private view returns (uint) {
         return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
     }
     
-    using address_make_payable for address;
-    function pickWinner() public restricted payable{
+    function pickWinner() public restricted {
         uint index = random() % players.length;
-        address payable winner = address(players[index]).make_payable();
-        payable(winner).transfer(address(this).balance);
-        players = new address[](0);
+        players[index].transfer(address(this).balance);
+        players = new address payable[](0);
     }
     
     modifier restricted() {
@@ -33,14 +31,8 @@ contract Lottery {
         _;
     }
     
-    function getPlayers() public view returns (address[] memory){
+    function getPlayers() public view returns (address payable[] memory) {
         return players;
     }
 
-}
-
-library address_make_payable {
-    function make_payable(address x) internal pure returns (address payable){
-        return address(uint160(x));
-    }
 }
